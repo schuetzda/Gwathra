d8386263ca68742a295967871eb1470f91b58107
David Schuetz <david-schuetz@gmx.de>
2021-04-09 14:43:33 +0200

    Other noise function

--- GwaRay/src/raymain.cpp
+++ GwaRay/src/raymain.cpp
@@ -23,7 +23,7 @@ namespace gwa {
 		screenVA.setArrayBuffer(0, GL_FLOAT, 2, 2, fullSreenTriangle, sizeof(float));
 		computeShader.create(rayTracerComputeShaderPath.c_str());
 		initComputeShaderTex();		
-		glBindImageTexture(0, tex_out, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+		glBindImageTexture(0, tex_out, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 	}
 	void RayMain::render() {
 		//View/Projection Matrices
@@ -83,8 +83,8 @@ namespace gwa {
 		glBindTexture(GL_TEXTURE_2D, tex_out);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, m_width, m_height, 0, GL_RGBA, GL_FLOAT,	NULL);
 
 		glEnable(GL_TEXTURE_2D);
